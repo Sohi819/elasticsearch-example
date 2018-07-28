e364329b495bd7e5d238c58588fc08f8f1952bee
xiaoniao <401111207@qq.com>
2018-07-28 09:43:34 +0800

    init springboot

---
+++ src/main/java/com/example/elasticsearchexample/ElasticsearchExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.elasticsearchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchExampleApplication.class, args);
+	}
+}
